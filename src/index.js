4125872bd3d6dcdcafd7267f28284655f54f29d7
Dong <dongntde160034@fpt.edu.vn>
2023-06-07 08:26:49 +0700

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const paragraph = React.createElement(
+  'p',
+  {},
+  'This is a paragraph without using JSX!'
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(paragraph);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
