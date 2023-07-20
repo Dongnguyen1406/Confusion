2b18d5b9d1937c11a25ace051078147f5700ff3b
Dong <dongntde160034@fpt.edu.vn>
2023-07-20 22:31:05 +0700

    Fetch from Server

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -3,12 +3,13 @@ import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({ dish }) {
   if (dish != null) {
     return (
       <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle><b>{dish.name}</b></CardTitle>
           <CardText>{dish.description}</CardText>
